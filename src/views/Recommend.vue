a2a8e5acc012b3287b102b5f51f515261e5827c0
hsimao <e087754958@gmail.com>
2021-06-01 21:26:04 +0800

    feat: get recommend api setting

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="recommend">
-    推薦頁面
-  </div>
+  <div class="recommend">推薦頁面</div>
 </template>
 
 <script>
+import { getRecommend } from '@/service/recommend'
+
 export default {
-  name: 'RecommendPage'
+  name: 'RecommendPage',
+  created() {
+    console.log('hello')
+
+    getRecommend()
+  }
 }
 </script>
 
