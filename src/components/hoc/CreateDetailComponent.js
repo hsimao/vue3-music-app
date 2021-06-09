e4a135b7363e59eb67750b111d6c10064c9e9282
hsimao <e087754958@gmail.com>
2021-06-09 19:24:49 +0800

    feat: 封裝音樂詳細頁面 hoc 組件

---
+++ src/components/hoc/CreateDetailComponent.js
@@ -0,0 +1,71 @@
+import MusicList from '@/components/MusicList/MusicList'
+import storage from 'good-storage'
+import { processSongs } from '@/service/song'
+
+export default function createDetailComponent({ name, storageKey, fetch }) {
+  return {
+    name,
+    components: {
+      MusicList
+    },
+    props: {
+      data: {
+        type: Object,
+        default: () => null
+      }
+    },
+    data() {
+      return {
+        songs: [],
+        loading: true
+      }
+    },
+    computed: {
+      cacheData() {
+        let result = null
+        const data = this.data
+
+        if (data) {
+          result = data
+        } else {
+          const cached = storage.session.get(storageKey)
+          if (cached?.mid === this.dataMid) {
+            result = cached
+          }
+        }
+
+        return result
+      },
+      pic() {
+        return this.cacheData?.pic
+      },
+      title() {
+        return this.cacheData?.name
+      },
+      dataMid() {
+        return this.$route.params.id
+      }
+    },
+    async created() {
+      await this.init()
+    },
+    methods: {
+      async init() {
+        const data = this.cacheData
+        if (!data) {
+          // 回到第一層路由
+          const firstLevelPath = this.$route.matched[0].path
+          return this.$router.push(firstLevelPath)
+        }
+        try {
+          const result = await fetch(data)
+          this.songs = await processSongs(result.songs)
+        } catch (err) {
+          console.log(err)
+        } finally {
+          this.loading = false
+        }
+      }
+    }
+  }
+}
