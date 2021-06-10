2d4f0cc0bb393ebf24cba382bfbb34b2fd64cd2f
hsimao <e087754958@gmail.com>
2021-06-10 13:28:57 +0800

    feat: search page search input

---
+++ src/components/Search/SearchInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="search-input">
+    <i class="icon-search" />
+    <input class="input-inner" :value="modelValue" @input="changeInput" />
+    <i class="icon-dismiss" v-show="modelValue" @click="clear" />
+  </div>
+</template>
+
+<script>
+import { debounce } from 'throttle-debounce'
+
+export default {
+  name: 'SearchInput',
+  props: {
+    modelValue: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    changeInput: debounce(300, function(e) {
+      const value = e.target.value.trim()
+      this.$emit('update:modelValue', value)
+    }),
+    clear() {
+      this.$emit('update:modelValue', '')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-input {
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0 6px;
+  height: 32px;
+  background: $color-highlight-background;
+  border-radius: 6px;
+  .icon-search {
+    font-size: 24px;
+    color: $color-text-d;
+  }
+  .input-inner {
+    flex: 1;
+    margin: 0 5px;
+    line-height: 18px;
+    background: $color-highlight-background;
+    color: $color-text;
+    font-size: $font-size-medium;
+    outline: 0;
+    &::placeholder {
+      color: $color-text-d;
+    }
+  }
+  .icon-dismiss {
+    font-size: 16px;
+    color: $color-text-d;
+  }
+}
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="search">
-    搜尋頁面
+    <div class="search-input-wrapper">
+      <SearchInput v-model="query" />
+      <pre>{{ query }}</pre>
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import SearchInput from '@/components/Search/SearchInput'
+
 export default {
-  name: 'SearchPage'
+  name: 'SearchPage',
+  components: {
+    SearchInput
+  },
+  setup() {
+    const query = ref('')
+
+    return {
+      query
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.search {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+
+  .search-input-wrapper {
+    margin: 20px;
+  }
+}
+</style>
