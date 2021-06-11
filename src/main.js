b2f872284d571e6e8778910c51d5a71a3b2abce0
hsimao <e087754958@gmail.com>
2021-06-11 15:10:24 +0800

    feat: 更新 localStore 內收藏、最近播放的 url

--- src/main.js
+++ src/main.js
@@ -5,9 +5,31 @@ import store from './store'
 import lazyPlugin from 'vue3-lazy'
 import loadingDirective from '@/components/base/Loading/directive'
 import noResultDirective from '@/components/base/NoResult/directive'
+import { load, saveAll } from '@/assets/js/array-store'
+import { FAVORITE_KEY, PLAY_KEY } from '@/assets/js/constant'
+import { processSongs } from '@/service/song'
 
 // 引入全域樣式
 import '@/assets/scss/index.scss'
+
+// 更新 localStore 內收藏歌曲的播放 url
+const favoriteSongs = load(FAVORITE_KEY)
+
+if (favoriteSongs.length > 0) {
+  processSongs(favoriteSongs).then(songs => {
+    store.commit('setFavoriteList', songs)
+    saveAll(songs, FAVORITE_KEY)
+  })
+}
+
+// 更新 localStore 播放紀錄中的播放 url
+const historySongs = load(PLAY_KEY)
+if (historySongs.length > 0) {
+  processSongs(historySongs).then(songs => {
+    store.commit('setPlayHistory', songs)
+    saveAll(songs, PLAY_KEY)
+  })
+}
 
 createApp(App)
   .use(store)
--- src/store/state.js
+++ src/store/state.js
@@ -1,21 +1,16 @@
-import {
-  PLAY_MODE,
-  FAVORITE_KEY,
-  SEARCH_KEY,
-  PLAY_KEY
-} from '@/assets/js/constant'
+import { PLAY_MODE, SEARCH_KEY } from '@/assets/js/constant'
 import { load } from '@/assets/js/array-store'
 
 const state = {
   sequenceList: [], // 原始曲目列表
   playlist: [], // 播放列表(順序可能是隨機)
-  favoriteList: load(FAVORITE_KEY), // 收藏歌曲列表
+  favoriteList: [], // 收藏歌曲列表
   playing: false,
   playMode: PLAY_MODE.sqeuence,
   currentIndex: 0,
   fullScreen: false,
   searchHistory: load(SEARCH_KEY),
-  playHistory: load(PLAY_KEY)
+  playHistory: []
 }
 
 export default state
